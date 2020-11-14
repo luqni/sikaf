171bfc6063ad7b8aeb6731ce9a06eb4413fd1476
luqni <mluqnibaehaqi@gmail.com>
2020-11-14 14:44:49 +0700

    change link

--- system/ktupad.js
+++ system/ktupad.js
@@ -39,7 +39,7 @@ var conf={
   data:{},
   mod:'view',
   model:'app.php',
-  host:'https://guru.sikaf.id/data/',
+  host:'https://guru.sikaf.id/',
   path:'ktupad/model.php',
   profile:{ nama:'ktupad',alamat:'jakarta',telpon:'0812.3123.7896',},
   induk:'1',
